8ad5a8ed80be401a8640d622e63ee2e7c7e33387
hazelnut6 <hazelpachoco6@gmail.com>
2025-05-06 12:17:51 +0800

    Add function for error

--- index.js
+++ index.js
@@ -11,6 +11,17 @@ app.use(express.urlencoded({ extended: true }));
 //ROUTERS
 app.use('/', indexRouter);
 
+//ERROR
+app.use((err, req, res, next) => {
+    console.log(err);
+    res.status(err.statusCode || 500).send(err.message);
+});
+    
+app.use((req, res) => {
+    res.status(404).render('404', { title: '404' });
+});
+
+
 //PORT
 const PORT = process.env.PORT;
 app.listen(PORT, () => { console.log('Running...') });
