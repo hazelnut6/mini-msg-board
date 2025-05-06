cc8320b47c1fec82b4f3415852d5cf2dfd7c2de8
hazelnut6 <hazelpachoco6@gmail.com>
2025-05-06 11:52:16 +0800

    Add id and create new route

--- routes/indexRouter.js
+++ routes/indexRouter.js
@@ -1,5 +1,6 @@
 const { Router } = require('express');
 const indexRouter = Router();
+const { v4: uuidv4 } = require('uuid');
 
 const links = [
     { href: '/', text: 'Home' },
@@ -8,11 +9,13 @@ const links = [
 
 const quotes = [
     {
+        id: uuidv4(),
         text: 'Don\'t look at me. I didn\'t put that clumsy glamour on him. Sigils are beneath me.',
         name: 'Agatha Harkness',
         added: new Date()
     },
     {
+        id: uuidv4(),
         text: 'What is grief, if not love persevering?',
         name: 'Vision',
         added: new Date()
@@ -31,12 +34,24 @@ indexRouter.post('/new', (req, res) => {
     const { name, text } = req.body;
 
     quotes.push({
+        id: uuidv4(),
         text: text,
         name: name,
         added: new Date()
     });
 
     res.redirect('/');
+});
+
+indexRouter.get('/quotes/:id', (req, res) => {
+    const quoteId = req.params.id;
+    const quote = quotes.find(q => q.id === quoteId);
+
+    if(quote) {
+        res.render('details', { title: 'Quote details', links, quote });
+    } else {
+        res.status(404).send('Quote not found');
+    }
 });
 
 module.exports = indexRouter;
